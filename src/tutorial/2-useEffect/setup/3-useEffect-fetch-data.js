059c58bff030d4771b57174e14aad062c336b927
Samuel Lee <lee.sam1492@gmail.com>
2022-01-24 14:25:43 -0700

    learning-react

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -8,16 +8,30 @@ const UseEffectFetchData = () => {
   const getUsers = async () => {
     const res = await fetch(url);
     const users = await res.json();
-    console.log(users);
+    setUsers(users);
+    // console.log(users);
   };
 
   useEffect(() => {
     getUsers();
-  });
+  }, []);
 
   return (
     <>
-      <h3>github users</h3>
+      {users.map((user) => {
+        const { id, login, avatar_url, html_url } = user;
+        return (
+          <ul className="users">
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>
+          </ul>
+        );
+      })}
     </>
   );
 };
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("peter");
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>Value: {secondValue}</h1> */}
+      <h1>{text || "john doe"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Toggle Error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
+      {/* {text && <h1>Hello world</h1>}
+      {!text && <h1>Hello world</h1>} */}
+    </>
+  );
 };
 
 export default ShortCircuit;
